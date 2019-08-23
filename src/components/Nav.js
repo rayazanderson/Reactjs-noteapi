8919dac40c957871d6385afc818110520336f913
rayazanderson <rayazand@gmail.com>
2019-08-23 13:04:57 -0700

    add components and nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/Nav';
+
+class Nav extends React.Component {
+ render() {
+   return (
+     <div className="App">
+        <Nav />
+
+
+
+     </div>
+   );
+ }
+}
+
+export default Nav;
