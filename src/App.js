15351dec271e064b36b130ce554efd8a481f7dc7
rayazanderson <rayazand@gmail.com>
2019-08-26 11:05:47 -0700

    add delete functions

--- src/App.js
+++ src/App.js
@@ -11,13 +11,15 @@ class App extends Component {
     super();
     this.state = {
       showNote: false,
-      notes: []
+      notes: [],
+      note: {}
     };
   }
 
   toggleNote = () => {
     this.setState({
-      showNote: !this.state.showNote
+      showNote: ! this.state.showNote,
+      note: {}
     });
   }
 
@@ -27,23 +29,51 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }
 
-  getNote = () => {
-    console.log('Clicked!');
+  getNote = (id) => {
+    axios.get(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ note: res.data, showNote: true }) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+    return axios.patch(urlFor(`notes/${id}`), data);
+  } else {
+    return axios.post(urlFor('notes'), data);
+    }
+  }
+
+
+  submitNote = (data, id) => {
+  this.performSubmissionRequest(data, id)
+  .then((res) => this.setState({ showNote: false }) )
+  .catch((err) => console.log(err.response.data) );
+  }
+
+  deleteNote = (id) => {
+    const newNotesState = this.state.notes.filter((note) => note.id !== id );
+    axios.delete(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ notes: newNotesState }))
+    .catch((err) => console.log(err.response.data) );
   }
 
   render() {
-    const { showNote, notes } = this.state;
+    const { showNote, notes, note } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
         {showNote ?
-          <Note />
+          <Note 
+            note={note}
+            submitNote={this.submitNote}
+          />
           :
           <List
             getNotes={this.getNotes}
             notes={notes}
             getNote={this.getNote}
+            deleteNote={this.deleteNote}
           />
         }
       </div>
