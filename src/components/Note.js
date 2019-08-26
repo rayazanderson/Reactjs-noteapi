a6dc0fd293b69aea9dedc686c43670e08daff5db
rayazanderson <rayazand@gmail.com>
2019-08-26 11:25:24 -0700

    adding newTag/showTag

--- src/components/Note.js
+++ src/components/Note.js
@@ -10,6 +10,22 @@ class Note extends React.Component {
       };
       this.props.submitNote(formData, this.props.note.id);
     }
+  
+  renderTagForm() {
+    if (!this.props.newTag) {
+      return (
+        <span>
+          Tag your note:
+          <i
+            className="tag-button material-icons"
+            onClick={() => this.props.showTagForm()}
+          >
+              add circle
+          </i>
+        </span>
+      );
+    }
+  }
 
   render() {
     const { note } = this.props;
@@ -33,6 +49,11 @@ class Note extends React.Component {
           />
           <input className="note-button" type="submit" value="Submit" />
         </form>
+        <div className="tag-container">
+          <div className="tag-button-container">
+            {this.renderTagForm()}
+          </div>
+        </div>  
       </div>
     );
   }
